e76e99ea2d61afe90f88f14968b0130da6466aa2
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-08 16:31:31 +0000

    Cálculo del valor energético en kJ Code. Funciona.

--- lib/gema/etiqueta.rb
+++ lib/gema/etiqueta.rb
@@ -4,4 +4,8 @@ class Etiqueta
   def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas, sal)
     @nombre, @grasas, @grasas_saturadas, @hidratos, @azucares, @proteinas, @sal = nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas, sal
   end
+
+  def valor_kJ
+    (@grasas*37)+(@hidratos*17)+(@proteinas*17)+(@sal*25)
+  end
 end
