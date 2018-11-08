2f8bb35024df191a9f6b3be115a416aceb4c5e30
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-08 16:10:09 +0000

    Sal Code. Funciona.

--- lib/gema/etiqueta.rb
+++ lib/gema/etiqueta.rb
@@ -1,7 +1,7 @@
 class Etiqueta
-  attr_reader :nombre, :grasas, :grasas_saturadas, :hidratos, :azucares, :proteinas
+  attr_reader :nombre, :grasas, :grasas_saturadas, :hidratos, :azucares, :proteinas, :sal
 
-  def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas)
-    @nombre, @grasas, @grasas_saturadas, @hidratos, @azucares, @proteinas = nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas
+  def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas, sal)
+    @nombre, @grasas, @grasas_saturadas, @hidratos, @azucares, @proteinas, @sal = nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas, sal
   end
 end
