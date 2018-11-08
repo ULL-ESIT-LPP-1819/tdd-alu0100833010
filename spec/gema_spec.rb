e98d8133c4b2cd154af56dfead6c1083c8d530ad
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-08 15:35:01 +0000

    Nombre etiqueta TDD. Falla.

--- spec/gema_spec.rb
+++ spec/gema_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Gema do
   it "does something useful" do
     expect(true).to eq(true)
   end
+
+  describe Etiqueta do
+    before :all do
+      @lata_de_atun = Etiqueta.new("Lata de atún")
+    end
+
+    context "Expectativas iniciales" do
+      it "Existe un nombre para la etiqueta" do
+	expect(@lata_de_atun.nombre).to eq("Lata de atún")
+      end
+    end
+  end
 end
