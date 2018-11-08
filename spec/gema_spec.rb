ef6962de39350a0aa4a864c7ef0217341714c423
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-08 19:40:08 +0000

    Añadiendo resultados del %IR.

--- spec/gema_spec.rb
+++ spec/gema_spec.rb
@@ -55,27 +55,27 @@ RSpec.describe Gema do
     end
 
     context "Cálculo de la Ingesta Recomendada" do
-      it "Ingesta recomendada para la grasa total" do
+      it "Ingesta recomendada para la grasa total" do			# El porcentaje de grasas está por debajo de la IR
         expect(@lata_de_atun.grasa_IR).to eq((23.0*100)/70)
       end
 
-      it "Ingesta recomendada para las grasas saturadas" do
+      it "Ingesta recomendada para las grasas saturadas" do		# El porcentaje de grasas saturadas está por debajo de la IR
         expect(@lata_de_atun.grasa_saturada_IR).to eq((2.7*100)/20)
       end
 
-      it "Ingesta recomendada para los hidratos de carbono" do
+      it "Ingesta recomendada para los hidratos de carbono" do		# El porcentaje de hidratos de carbono está por debajo de la IR
         expect(@lata_de_atun.hidratos_IR).to eq((0.5*100)/260)
       end
 
-      it "Ingesta recomendada para los azúcares" do
+      it "Ingesta recomendada para los azúcares" do			# El porcentaje de azúcares está por debajo de la IR
         expect(@lata_de_atun.azucar_IR).to eq((0.5*100)/90)
       end
 
-      it "Ingesta recomendada para las proteínas" do
+      it "Ingesta recomendada para las proteínas" do			# El porcentaje de proteínas está por debajo de la IR
         expect(@lata_de_atun.proteinas_IR).to eq((20.0*100)/50)
       end
 	
-      it "Ingesta recomendada para la sal" do
+      it "Ingesta recomendada para la sal" do				# El porcentaje de sal está por debajo de la IR
         expect(@lata_de_atun.sal_IR).to eq((0.40*100)/6)
       end
     end
