b4f3ec9e26bec4f6ff53fb70ba8090de44a58bb5
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-14 18:53:57 +0000

    Existe una lista vacía CODE. Funciona.

---
+++ lib/gema/lista.rb
@@ -0,0 +1,19 @@
+Node = Struct.new(:value, :next, :prev)
+
+class Lista
+  attr_accessor :head, :tail, :size
+ 
+  def initialize()
+    @head = Node.new(nil, nil, nil)
+    @tail = Node.new(nil, nil, nil)
+    @size = 0
+  end
+ 
+  def empty()
+    if (@size == 0)
+      return true
+    else
+      return false
+    end
+  end
+end
