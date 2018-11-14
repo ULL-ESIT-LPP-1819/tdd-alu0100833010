675e25a68a3dc9e5f0bffbdc17385e47d8921ff6
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-14 19:49:18 +0000

    Existe un metodo para insertar elementos CODE. Funciona.

--- lib/gema/lista.rb
+++ lib/gema/lista.rb
@@ -16,4 +16,24 @@ class Lista
       return false
     end
   end
+
+  def insertar_tail(valor)
+    elemento = Node.new(valor, @head, nil)
+    if (@size == 0)
+      @tail = Node.new(valor, nil, nil)
+      @head = @tail
+      @size += 1
+    elsif (@size == 1)
+      @tail = elemento
+      @head.next = @tail
+      @tail.prev = @head
+      @size += 1
+    elsif (@size > 1)
+      aux = Node.new(@tail.value, @tail.next, elemento)
+      aux.prev.next = aux
+      @tail = elemento
+      @tail.next.prev = @tail
+      @size += 1
+    end
+  end
 end
