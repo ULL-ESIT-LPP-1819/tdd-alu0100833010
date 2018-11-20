132aea51c2b64a75eddad96b4bdb1aa6e322e1c3
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-20 22:37:08 +0000

    Individuo to_s CODE. Funciona.

--- lib/gema/individuo.rb
+++ lib/gema/individuo.rb
@@ -5,4 +5,7 @@ class Individuo
     @n_historia, @nombre, @apellido, @edad, @sexo, @nacimiento, @ocupacion, @fumador = n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador
   end
 
+  def to_s
+    "[Número de historia=#{@n_historia}, Nombre=#{@nombre}, Apellido=#{@apellido}, Edad=#{@edad}, Sexo=#{@sexo}, Nacimiento=#{@nacimiento}, Ocupación=#{@ocupacion}, Fumador=#{@fumador}]:"
+  end
 end
