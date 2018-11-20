809bf3685bdc43f1005e002593a5109f41c318ca
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-20 22:06:32 +0000

    Creando individuo 1 TDD. Falla.

--- spec/gema_clase_spec.rb
+++ spec/gema_clase_spec.rb
@@ -1,3 +1,9 @@
-require 'spec_helper.rb'
+require "spec_helper"
+
+RSpec.describe Individuo do
+  before :each do
+    @individuo1 = Individuo.new(1, "Sara", "Pérez", 23, 0, "17/08/1995", "Estudiante", "No") 
+  end
+end
 
 
