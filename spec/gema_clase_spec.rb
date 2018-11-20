ecc5b5105cc5006ef620864bf88f096bc616cd91
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-20 23:10:58 +0000

    Comprobando tipo, clase y jerarquía de los objetos de la clase Individuo.

--- spec/gema_clase_spec.rb
+++ spec/gema_clase_spec.rb
@@ -10,6 +10,23 @@ RSpec.describe Individuo do
       expect(@individuo1.to_s).to eq("[Número de historia=#{@individuo1.n_historia}, Nombre=#{@individuo1.nombre}, Apellido=#{@individuo1.apellido}, Edad=#{@individuo1.edad}, Sexo=#{@individuo1.sexo}, Nacimiento=#{@individuo1.nacimiento}, Ocupación=#{@individuo1.ocupacion}, Fumador=#{@individuo1.fumador}]:") 
     end
   end
+  
+  context "Pruebas para comprobar la clase de un objeto, el tipo de un objeto y su pertenecia a una jerarquı́a" do
+    it "Existe un método para comprobar el tipo de objeto" do
+      expect(@individuo1.instance_of? Individuo).to eq(true)
+    end
+
+    it "Existe un método para comprobar la clase de un objeto" do
+      expect(@individuo1.class).to eq(Individuo)
+      expect(@individuo1.is_a? Individuo).to eq(true)
+    end
+
+    it "Existe un método para comprobar la jerarquía de un objeto" do
+      expect(@individuo1.kind_of? Individuo).to eq(true)
+      expect(@individuo1.kind_of? BasicObject).to eq(true)
+      expect(@individuo1.kind_of? Object).to eq(true)
+    end
+  end
 end
 
 
