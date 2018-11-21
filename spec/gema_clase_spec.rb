a921a8b695d7814570d96ad51aaefa7fc30cf2d5
Sara Pérez <alu0100833010@ull.edu.es>
2018-11-21 12:08:21 +0000

    Arreglando fallos con listas y aplicando jerarquías, tipos y objetos a todas las clases

--- spec/gema_clase_spec.rb
+++ spec/gema_clase_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe Individuo do
   context "Pruebas para comprobar la clase de un objeto, el tipo de un objeto y su pertenecia a una jerarquı́a" do
     it "Existe un método para comprobar el tipo de objeto" do
       expect(@individuo1.instance_of? Individuo).to eq(true)
+      expect(@individuo1.instance_of? Object).not_to eq(true)
+      expect(@individuo1.instance_of? BasicObject).not_to eq(true)
+      expect(@individuo1).to respond_to(:n_historia, :nombre, :apellido, :edad, :sexo, :nacimiento, :ocupacion, :fumador)
+      expect(@individuo1).to respond_to('to_s')
     end
 
     it "Existe un método para comprobar la clase de un objeto" do
@@ -61,6 +65,13 @@ RSpec.describe Individuo do
     context "Pruebas para comprobar la clase de un objeto, el tipo de un objeto y su pertenecia a una jerarquı́a" do
       it "Existe un método para comprobar el tipo de objeto" do
         expect(@individuo1.instance_of? Paciente).to eq(true)
+        expect(@individuo1.instance_of? Object).not_to eq(true)
+        expect(@individuo1.instance_of? BasicObject).not_to eq(true)
+        expect(@individuo1).to respond_to(:n_historia, :nombre, :apellido, :edad, :sexo, :nacimiento, :ocupacion, :fumador, :peso, :talla, :cintura, :cadera, :tricipital, :bicipital, :subescapular, :suprailiaco)
+        expect(@individuo1).to respond_to('to_s')
+        expect(@individuo1).to respond_to('imc')
+        expect(@individuo1).to respond_to('masa')
+        expect(@individuo1).to respond_to('rcc')
       end
 
       it "Existe un método para comprobar la clase de un objeto" do
@@ -74,47 +85,104 @@ RSpec.describe Individuo do
         expect(@individuo1.kind_of? Paciente).to eq(true)
         expect(@individuo1.kind_of? BasicObject).to eq(true)
         expect(@individuo1.kind_of? Object).to eq(true)
-      end
+      end     
     end
   end
   describe Lista do
-    before :each do
-      @lista_p = Lista.new()
-      @individuo1 = Paciente.new(1, "Sara", "Pérez", 23, 0, "17/08/1995", "Estudiante", "No", 60.0, 1.64, [67.6,68.1], [93.6,94.1], [1.2,2.2,1.3], [1.1,1.9,1.3], [1.4,2.1,1.6], [1.2,2.8,2.3])
-      @individuo2 = Paciente.new(2, "María", "García", 33, 0, "13/01/1985", "Profesora", "Si", 110, 1.67, [83.4,83.8], [105.1,105.6], [1.5,2.8,1.9], [1.6,1.2,1.9], [1.1,2.9,1.3], [1.6,2.2,2.2])
-      @individuo3 = Paciente.new(3, "Juan", "Castro", 53, 1, "04/10/1965", "Ingeniero", "No", 83, 1.78, [68.7,69.0], [71.5,71.3], [1.4,2.6,1.7], [1.8,1.1,1.6], [2.4,1.1,2.6], [2.2,1.8,1.3])
-      @individuo4 = Paciente.new(4, "Lorena", "Rodríguez", 22, 0, "12/05/1996", "Estudiante", "Si", 47, 1.61, [60.0,60.9], [90.0,90.5], [1.2,2.2,1.3], [1.1,1.9,1.3], [1.4,2.1,1.6], [1.2,2.8,2.3])
-      @individuo5 = Paciente.new(5, "Carlos", "Coello", 13, 1, "15/06/2005", "Estudiante", "No", 58, 1.68, [60.0,60.9], [90.0,90.5], [1.2,2.2,1.3], [1.1,1.9,1.3], [1.4,2.1,1.6], [1.2,2.8,2.3])
+    before :all do
+      @lista = Lista.new()
+      @individuo2 = Paciente.new(2, "Sara", "Pérez", 23, 0, "17/08/1995", "Estudiante", "No", 60.0, 1.64, [67.6,68.1], [93.6,94.1], [1.2,2.2,1.3], [1.1,1.9,1.3], [1.4,2.1,1.6], [1.2,2.8,2.3])
+      @individuo3 = Paciente.new(3, "María", "García", 33, 0, "13/01/1985", "Profesora", "Si", 110, 1.67, [83.4,83.8], [105.1,105.6], [1.5,2.8,1.9], [1.6,1.2,1.9], [1.1,2.9,1.3], [1.6,2.2,2.2])
+      @individuo4 = Paciente.new(4, "Juan", "Castro", 53, 1, "04/10/1965", "Ingeniero", "No", 83, 1.78, [68.7,69.0], [71.5,71.3], [1.4,2.6,1.7], [1.8,1.1,1.6], [2.4,1.1,2.6], [2.2,1.8,1.3])
+      @individuo5 = Paciente.new(5, "Lorena", "Rodríguez", 22, 0, "12/05/1996", "Estudiante", "Si", 47, 1.61, [60.0,60.9], [90.0,90.5], [1.2,2.2,1.3], [1.1,1.9,1.3], [1.4,2.1,1.6], [1.2,2.8,2.3])
+      @individuo6 = Paciente.new(6, "Carlos", "Coello", 13, 1, "15/06/2005", "Estudiante", "No", 58, 1.68, [60.0,60.9], [90.0,90.5], [1.2,2.2,1.3], [1.1,1.9,1.3], [1.4,2.1,1.6], [1.2,2.8,2.3])
     end
     
-    it "Existe una lista vacía" do
-      expect(@lista_p.empty?).to eq(true)
-    end
+    describe "Expectativas generales" do
+      it "Existe una lista vacía" do
+        expect(@lista.empty?).to eq(true)
+      end
 
-    it "Existe un valor para head nulo" do
-      expect(@lista_p.head.value).to eq(nil)
-    end
-
-    it "Existe un valor para tail nulo" do
-      expect(@lista_p.tail.value).to eq(nil)
-    end
-
-    it "Existe un tamaño para la lista igual a 0" do
-      expect(@lista_p.size).to eq(0)
-    end   
-    
-    context "Insertando elementos" do
-      it "Existen los nodos prev y next vacíos" do
-        expect(@lista_p.head.next).to eq(nil)
-        expect(@lista_p.tail.prev).to eq(nil)
+      it "Existe un método para insertar elementos" do
+        expect(@lista.push_elemento(@individuo2)).to eq(@individuo2)
+        expect(@lista.push_elemento(@individuo3)).to eq(@individuo3)
+        expect(@lista.push_elemento(@individuo4)).to eq(@individuo4)
+        expect(@lista.push_elemento(@individuo5)).to eq(@individuo5)
+        expect(@lista.push_elemento(@individuo6)).to eq(@individuo6)
       end
       
-      it "Existe un método para insertar elementos" do
-        expect(@lista_p.push_elemento(@individuo2)).to eq(@individuo2)
+      it "La lista ya no es vacía" do
+        expect(@lista.empty?).to eq(false)
       end
-      it "Ya no existe una lista vacía" do
-        expect(@lista_p.empty?).to eq(false)
-      end 
+      
+      it "Head es el primer elemento insertado" do
+        expect(@lista.head.value).to eq(@individuo2)
+      end
+
+      it "Tail es el último elemento insertado" do
+        expect(@lista.tail.value).to eq(@individuo6)
+      end
+    end
+    context "Clasificación de los pacientes según su IMC" do
+     # it "Accediendo al IMC de los pacientes" do
+       # expect(@lista.pop_elemento().imc).to eq(@individuo2.imc)
+     # end
+      it "Comprobando clasificación de la OMS" do
+        expect(@lista.pop_elemento().imc).to be <= 25.0 #Peso adecuado. No recibe tratamiento.
+        expect(@lista.pop_elemento().imc).to be >= 35.0 #Obesidad grado 2. Recibe tratamiento.
+        expect(@lista.pop_elemento().imc).to be >= 25.0 #Sobrepeso. Recibe tratamiento.
+        expect(@lista.pop_elemento().imc).to be <= 18.5 #Bajo peso. Recibe tratamiento.
+        expect(@lista.pop_elemento().imc).to be <= 25.0 #Peso adecuado. No recibe tratamiento.
+      end
+    end
+    context "Pruebas para comprobar la clase de un objeto, el tipo de un objeto y su pertenecia a una jerarquı́a" do
+      it "Existe un método para comprobar el tipo de objeto" do
+        expect(@lista.instance_of? Lista).to eq(true)
+        expect(@lista.instance_of? Object).not_to eq(true)
+        expect(@lista.instance_of? BasicObject).not_to eq(true)
+        expect(@lista).to respond_to(:head, :tail, :size)
+        expect(@lista).to respond_to('empty?')
+        expect(@lista).to respond_to('push_elemento')
+        expect(@lista).to respond_to('pop_elemento')
+      end
+
+      it "Existe un método para comprobar la clase de un objeto" do
+        expect(@lista.class).to eq(Lista)
+        expect(@lista.is_a? Lista).to eq(true)
+      end
+
+      it "Existe un método para comprobar la jerarquía de un objeto" do
+        expect(@lista.kind_of? Lista).to eq(true)
+        expect(@lista.kind_of? BasicObject).to eq(true)
+        expect(@lista.kind_of? Object).to eq(true)
+      end
+    end
+  end
+  describe Etiqueta do
+    before :each do
+      @lata_de_atun = Etiqueta.new("Lata de atún", 23.0, 2.7, 0.5, 0.5, 20.0, 0.40, [0])
+    end 
+    context "Pruebas para comprobar la clase de un objeto, el tipo de un objeto y su pertenecia a una jerarquı́a" do
+      it "Existe un método para comprobar el tipo de objeto" do
+        expect(@lata_de_atun.instance_of? Etiqueta).to eq(true)
+        expect(@lata_de_atun.instance_of? Object).not_to eq(true)
+        expect(@lata_de_atun.instance_of? BasicObject).not_to eq(true)
+        expect(@lata_de_atun).to respond_to(:nombre, :grasas, :grasas_saturadas, :hidratos, :azucares, :proteinas, :sal)
+        expect(@lata_de_atun).to respond_to('valor_kJ')
+        expect(@lata_de_atun).to respond_to('valor_kcal')
+        expect(@lata_de_atun).to respond_to('to_s')
+      end
+
+      it "Existe un método para comprobar la clase de un objeto" do
+        expect(@lata_de_atun.class).to eq(Etiqueta)
+        expect(@lata_de_atun.is_a? Etiqueta).to eq(true)
+      end
+
+      it "Existe un método para comprobar la jerarquía de un objeto" do
+        expect(@lata_de_atun.kind_of? Etiqueta).to eq(true)
+        expect(@lata_de_atun.kind_of? BasicObject).to eq(true)
+        expect(@lata_de_atun.kind_of? Object).to eq(true)
+      end
     end
   end
 end
