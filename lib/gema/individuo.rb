8a787498ad3ba0b90b7bf8cde785ba8959fd9b9c
Sara Pérez <alu0100833010@ull.edu.es>
2018-12-06 12:29:53 +0000

    Módulo Comparable Individuo. Code. Funciona.

--- lib/gema/individuo.rb
+++ lib/gema/individuo.rb
@@ -1,60 +1,66 @@
-class Individuo
-  attr_accessor :n_historia, :nombre, :apellido, :edad, :sexo, :nacimiento, :ocupacion, :fumador
+module Individuo
+
+  class Individuo
+
+    include Comparable    
+
+    attr_accessor :n_historia, :nombre, :apellido, :edad, :sexo, :nacimiento, :ocupacion, :fumador
   
-  def initialize (n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador)
-    @n_historia, @nombre, @apellido, @edad, @sexo, @nacimiento, @ocupacion, @fumador = n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador
+    def initialize (n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador)
+      @n_historia, @nombre, @apellido, @edad, @sexo, @nacimiento, @ocupacion, @fumador = n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador
+    end
+
+    def to_s
+      "[Número de historia=#{@n_historia}, Nombre=#{@nombre}, Apellido=#{@apellido}, Edad=#{@edad}, Sexo=#{@sexo}, Nacimiento=#{@nacimiento}, Ocupación=#{@ocupacion}, Fumador=#{@fumador}]:"
+    end
   end
 
-  def to_s
-    "[Número de historia=#{@n_historia}, Nombre=#{@nombre}, Apellido=#{@apellido}, Edad=#{@edad}, Sexo=#{@sexo}, Nacimiento=#{@nacimiento}, Ocupación=#{@ocupacion}, Fumador=#{@fumador}]:"
-  end
-end
+  class Paciente < Individuo
+    attr_accessor :peso, :talla
 
-class Paciente < Individuo
-  attr_accessor :peso, :talla
-
-  def initialize (n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador, peso, talla, cintura, cadera, tricipital, bicipital, subescapular, suprailiaco)
-    super(n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador)
-    @peso, @talla, @cintura, @cadera, @tricipital, @bicipital, @subescapular, @suprailiaco = peso, talla, cintura, cadera, tricipital, bicipital, subescapular, suprailiaco
-  end
+    def initialize (n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador, peso, talla, cintura, cadera, tricipital, bicipital, subescapular, suprailiaco)
+      super(n_historia, nombre, apellido, edad, sexo, nacimiento, ocupacion, fumador)
+      @peso, @talla, @cintura, @cadera, @tricipital, @bicipital, @subescapular, @suprailiaco = peso, talla, cintura, cadera, tricipital, bicipital, subescapular, suprailiaco
+    end
  
-  def imc 
-    @peso/(@talla*@talla)
-  end
+    def imc 
+      @peso/(@talla*@talla)
+    end
 
-  def masa
-    1.2 * imc + 0.23 * @edad - 10.8 * @sexo - 5.4
-  end
+    def masa
+      1.2 * imc + 0.23 * @edad - 10.8 * @sexo - 5.4
+    end
 
-  def cintura
-    (@cintura[0]+@cintura[1])/2
-  end
+    def cintura
+      (@cintura[0]+@cintura[1])/2
+    end
 
-  def cadera
-    (@cadera[0]+@cadera[1])/2
-  end
+    def cadera
+      (@cadera[0]+@cadera[1])/2
+    end
 
-  def rcc
-    cintura/cadera
-  end
+    def rcc
+      cintura/cadera
+    end
 
-  def tricipital
-    (@tricipital[0]+@tricipital[1]+@tricipital[2])/3
-  end
+    def tricipital
+      (@tricipital[0]+@tricipital[1]+@tricipital[2])/3
+    end
 
-  def bicipital
-    (@bicipital[0]+@bicipital[1]+@bicipital[2])/3
-  end
+    def bicipital
+      (@bicipital[0]+@bicipital[1]+@bicipital[2])/3
+    end
 
-  def subescapular
-    (@subescapular[0]+@subescapular[1]+@subescapular[2])/3
-  end
+    def subescapular
+      (@subescapular[0]+@subescapular[1]+@subescapular[2])/3
+    end
 
-  def suprailiaco
-    (@suprailiaco[0]+@suprailiaco[1]+@suprailiaco[2])/3
-  end
+    def suprailiaco
+      (@suprailiaco[0]+@suprailiaco[1]+@suprailiaco[2])/3
+    end
  
-  def to_s
-    "[Peso=#{@peso}, Talla=#{@talla}, IMC=#{imc}, %MC=#{masa}, RCC=#{rcc}, Pliegues=#{tricipital},#{bicipital},#{subescapular},#{suprailiaco}]:"
-  end  
+    def to_s
+      "[Peso=#{@peso}, Talla=#{@talla}, IMC=#{imc}, %MC=#{masa}, RCC=#{rcc}, Pliegues=#{tricipital},#{bicipital},#{subescapular},#{suprailiaco}]:"
+    end  
+  end
 end
